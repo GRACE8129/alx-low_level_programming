c05416bd57637fc3413eb51870774e34adc652da
Osimen Grace <osimenobehi6@gmail.com>
2023-10-05 05:28:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a program that prints a line with put function
+ * Return:o(a success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle"\n);
+return (o);
+}
