1580379542102af78a21b852fe7fe3f9666d78ad
Osimen Grace <osimenobehi6@gmail.com>
2023-10-06 06:04:25 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	g = n % 10;
 	if (g > 5)
+	{
 		printf("last digit of %d is %d and is greater than 5\n", n, g);
+	}
 	if (g == 0)
+	{
 		printf("last digit of %d is %d and is 0\n", n, g);
+	}
 	if (g < 6 && g != 0)
+	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, g);
+	}
 	return (0);
 }
