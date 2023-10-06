15938a51864158f2708f5ef543517f5397d806f2
Osimen Grace <osimenobehi6@gmail.com>
2023-10-06 08:40:59 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+char j;
+
+for (j = '0'; j <= '9'; j++)
+
+putchar(j);
+putchar('\n');
+
+return (0);
+}
