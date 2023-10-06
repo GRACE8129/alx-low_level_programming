89ac4620c2749123bd69760a8eee7a541d8bc050
Osimen Grace <osimenobehi6@gmail.com>
2023-10-06 10:20:33 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 int n;
-int m;
 
-for (n = '0'; n <= '9'; n++)
+for (n = 48; n <= 57; n++)
 putchar(n);
-for (m = 'a'; m <= 'f'; n++)
-putchar(m);
+
+for (n = 97; n <= 102; n++)
+putchar(n);
+
 putchar('\n');
+
 return (0);
 }
