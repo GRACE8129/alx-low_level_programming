6780d26b7cc54fcaff7bbb160099772c0b9e7126
Osimen Grace <osimenobehi6@gmail.com>
2023-10-06 06:09:58 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, g);
 	}
-	if (g == 0)
+	else if (g == 0)
 	{
 		printf("last digit of %d is %d and is 0\n", n, g);
 	}
-	if (g < 6 && g != 0)
+	else (g < 6 && g != 0)
 	{
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, g);
 	}
