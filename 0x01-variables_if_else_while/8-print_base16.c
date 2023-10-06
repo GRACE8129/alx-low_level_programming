78e0ceeb31b1313fab74253ac5cbd261673b39c1
Osimen Grace <osimenobehi6@gmail.com>
2023-10-06 10:06:04 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+int n;
+int m;
+
+for (n = '0'; n <= '9'; n++)
+putchar(n);
+for (m = 'a'; m <= 'f'; n++)
+putchar(m);
+putchar('\n');
+return (0);
+}
