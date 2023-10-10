bcb4aebafaf78d8cd900abb3b0b6e0fa918e0025
osimen grace <osimenobehi6@gmail.com>
2023-10-10 10:42:19 -0700

    Recent push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	char j;
+	{
+
+	while (c <= 9)
+
+		for (j = 'a'; j <= 'z'; j++)
+
+			_putchar(j);
+
+	_putchar('\n');
+	c++;
+	}
+
+
+}
+
