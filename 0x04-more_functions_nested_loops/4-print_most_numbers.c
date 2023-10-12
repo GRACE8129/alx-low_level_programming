9c37e69fbe206f38cbdfa8e1db7fd93612916872
osimen grace <osimenobehi6@gmail.com>
2023-10-12 08:52:20 -0700

    Recent push

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - print 0 to 9 excluding 2 and 4
+  *Return: void
+  */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+	if (j != 2 && j != 4)
+		_putchar(j + '0');
+	}
+_putchar('\n');
+}
