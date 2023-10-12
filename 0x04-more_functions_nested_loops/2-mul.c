e76be02eb9c94e6196ad12be5fe665c3e92488fa
osimen grace <osimenobehi6@gmail.com>
2023-10-12 05:47:41 -0700

    Recent push

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul - multiplication of two integers
+  * @a: parameter 1
+  * @b: parameter 2
+  * Return: a * b
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
