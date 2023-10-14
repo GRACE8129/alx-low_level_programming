3deffab90929e65b6b631b5884ac0bb491db0ecf
osimen grace <osimenobehi6@gmail.com>
2023-10-14 05:34:26 -0700

    Recent push

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 		{
 			printf(" FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
@@ -31,5 +31,6 @@ int main(void)
 			printf(" %d", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
