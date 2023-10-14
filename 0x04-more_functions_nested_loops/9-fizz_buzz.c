74c46cf8ffa564b175bc27a0ab0f25f7be642e60
osimen grace <osimenobehi6@gmail.com>
2023-10-14 05:06:09 -0700

    Recent push

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+  * main - a program that prints 1 - 100 while adding fizz buss
+  * for multiples of 5 and 3
+  * Return: 0 (successful)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
