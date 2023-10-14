8a3b2169e14e974b098829ddaac58099a3eff679
osimen grace <osimenobehi6@gmail.com>
2023-10-14 06:43:24 -0700

    Recent push

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_triangle - to print a triangle else space
+  * @size: parameter function
+  * Return: void
+  */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
