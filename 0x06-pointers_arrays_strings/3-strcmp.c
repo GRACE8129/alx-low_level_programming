f301d9e917a69a78d3b8a296aae500ca880d2adf
osimen grace <osimenobehi6@gmail.com>
2023-10-18 14:40:38 -0700

    comparing 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int flag = 0;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] != s2[i])
-		{
-			flag = 1;
-			break;
-		}
+		s1++;
+		s2++;
 	}
-	if (flag == 0)
-	{
-		return 0;
-	}
-	return (int)(s1[i]) - (int)(s2[i]);
+	return (*s1 - *s2);
 }
